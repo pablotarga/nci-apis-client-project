84e09192d8024b26201d4424f3a66513ea1689fc
Pablo Targa <pablo.targa@gmail.com>
2020-04-07 21:29:51 +0100

    Change title style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { SystemService } from './services/system.service';
+import { LoginForm } from './interfaces/login-form';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
 
   constructor(public s: SystemService) { }
   title = 'NCI API CA3';
   subtitle = 'Bank Account HtmlClient';
+
+  login() {
+    this.s.login({} as LoginForm);
+  }
 }
