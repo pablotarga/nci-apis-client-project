8df1892ebda134768a7f3aa89eba9932df9ed4c5
Pablo Targa <pablo.targa@gmail.com>
2020-04-07 20:02:41 +0100

    Add login form and register component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import { AuthorizedComponent } from './authorized/authorized.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthorizedComponent
+    AuthorizedComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     FontAwesomeModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { SystemService } from '../services/system.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  form = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+
+  constructor(private fb: FormBuilder, public s: SystemService) { }
+  ngOnInit(): void { }
+
+  get email(): any { return this.form.get('email'); }
+  get emailIsInvalid(): any {
+    const e = this.email;
+    return e.invalid && (e.dirty || e.touched);
+  }
+
+  get password(): any { return this.form.get('password'); }
+  get passwordIsInvalid(): any {
+    const e = this.password;
+    return e.invalid && (e.dirty || e.touched);
+  }
+}
