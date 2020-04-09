187302b854f34d68f9245168b8b33fd52790755f
Pablo Targa <pablo.targa@gmail.com>
2020-04-09 19:04:12 +0100

    Removed flip to prepare for animations

--- src/app/account-detail/account-detail.component.ts
+++ src/app/account-detail/account-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, SimpleChange, OnChanges } from '@angular/core';
 import { Account } from '../interfaces/account';
 import { SystemService } from '../services/system.service';
 
@@ -7,11 +7,24 @@ import { SystemService } from '../services/system.service';
   templateUrl: './account-detail.component.html',
   styleUrls: ['./account-detail.component.scss']
 })
-export class AccountDetailComponent implements OnInit {
+export class AccountDetailComponent implements OnInit, OnChanges {
   @Input() account: Account;
+  acc: Account;
   constructor(public s: SystemService) { }
 
   ngOnInit(): void {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    const account: SimpleChange = changes.account;
+    if (account.currentValue) {
+      this.acc = account.currentValue;
+    }
+  }
+
+
+  close() {
+    this.s.activeAccount = null;
   }
 
 }
